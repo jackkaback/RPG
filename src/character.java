cdbbea66ce2bf9ad0928c5eb999b6f700f1e7dd5
jack <jackkaback@gmail.com>
2019-01-07 06:39:21 -0500

    adding stuff to character class

--- src/character.java
+++ src/character.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class character {
+
+	private Scanner input = new Scanner(System.in);
 
 	public String name;
 	public int defence;
@@ -7,7 +12,12 @@ public class character {
 
 	public int level = 1;
 	public int Exp;
+
+	//ammont of points to distribute on level up
 	public int lvlPoints;
+
+	//threshold to level up
+	private int expThreshold;
 
 
 	public int maxHealth;
@@ -18,7 +28,7 @@ public class character {
 	public int gold;
 
 	//initializer
-	public character(int defence, int attack, int Health, int luck, int mana, int Exp, String name){
+	public character(int defence, int attack, int Health, int luck, int mana, int Exp, String name) {
 		this.name = name;
 
 		this.Exp = Exp;
@@ -40,8 +50,41 @@ public class character {
 		levelUp();
 	}
 
-	//Levels up the character based on XP and level
-	public void levelUp(){
+	//adds experience points
+	public void addExp(int newExp) {
+		Exp += newExp;
+		levelUp();
+	}
 
+
+	//allows the distrobution of level up points
+	private void addLvlPoints() {
+
+		if (level % 10 == 0) {
+			lvlPoints = 5;
+		} else if (level % 5 == 0) {
+			lvlPoints = 3;
+		} else {
+			lvlPoints = 2;
+		}
+
+	}
+
+
+	//Levels up the character based on XP and level
+	private void levelUp() {
+
+		while (true) {
+			expThreshold = Math.pow(1.5, level) * 100;
+			if (Exp >= expThreshold) {
+				Exp -= expThreshold;
+				level++;
+				System.out.println("You leveled up.");
+				addLvlPoints();
+			} else {
+				break;
+			}
+
+		}
 	}
 }
