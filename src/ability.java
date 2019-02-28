2d483eefcc1125b518bbb9198eb5bbcd33c66b30
jack <jackkaback@gmail.com>
2019-02-28 16:00:33 -0500

    adding confusion

--- src/ability.java
+++ src/ability.java
@@ -1,6 +1,15 @@
 public class ability{
-	//TODO evertything involving this stuff
-	public ability(){
 
+	public int turnsRunning = 0;
+	public int maxTurns;
+	public int minTurns;
+	public int chanceToCast;
+	public String name;
+
+	//TODO evertything involving this stuff
+	public ability(int minTurns, int chanceToCast){
+		this.minTurns = minTurns;
+		maxTurns = (int) 2.5 * minTurns;
+		this.chanceToCast = chanceToCast;
 	}
 }
---
+++ src/confusion.java
@@ -0,0 +1,9 @@
+public class confusion extends ability{
+	
+	public confusion(){
+		minTurns = 2;
+		maxTurns = (int) 2.5 * minTurns;
+		chanceToCast = 50;
+		name = "Confusion";
+	}
+}
